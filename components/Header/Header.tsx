76530bc6de285b8afdc22fb78ac4d2f11642f7d5
thepicture <85333076+thepicture@users.noreply.github.com>
2022-08-07 16:36:14 +0500

    feat(auth): add sign out

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,13 +1,29 @@
 import React from "react";
 
+import Link from "next/link";
+
 import { Card, Typography } from "@mui/material";
+
+import { StyledLink } from "@pages";
+import { RELATIVE_SIGN_OUT_URL } from "@constants";
 
 export const Header = () => {
   return (
-    <Card elevation={16} sx={{ p: 2, gridArea: "h" }}>
-      <Typography component="h1" variant="h6">
+    <Card
+      elevation={16}
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        p: 2,
+        gridArea: "h",
+      }}
+    >
+      <Typography alignSelf="center" component="h1" variant="h6">
         Pictures
       </Typography>
+      <Link href={RELATIVE_SIGN_OUT_URL}>
+        <StyledLink style={{ width: "auto" }}>Sign out</StyledLink>
+      </Link>
     </Card>
   );
 };
